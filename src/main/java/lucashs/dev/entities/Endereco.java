6a0ef7ad64bdc707faa033b8d8d40d12cea66316
Lucas HS <lucashilles@gmail.com>
2025-03-17 22:19:54 -0300

    Create entity Endereco

---
+++ src/main/java/lucashs/dev/entities/Endereco.java
@@ -0,0 +1,31 @@
+package lucashs.dev.entities;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="endereco")
+public class Endereco extends PanacheEntityBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "end_id")
+    public int id;
+
+    @Column(name = "end_tipo_logradouro", nullable = false, length = 50)
+    public String tipoLogradouro;
+
+    @Column(name = "end_logradouro", nullable = false, length = 200)
+    public String logradouro;
+
+    @Column(name = "end_numero", nullable = false)
+    public int numero;
+
+    @Column(name = "end_bairro", nullable = false, length = 100)
+    public String bairro;
+
+    @ManyToOne
+    @JoinColumn(name = "cid_id", nullable = false)
+    public Cidade cidade;
+
+}
