41568db9b9f99b92c4ae60b3990b2850325b7770
Lucas HS <lucashilles@gmail.com>
2025-03-21 21:16:05 -0300

    Update CidadeRpository to implements PanacheRepositoryBase

--- src/main/java/lucashs/dev/repositories/CidadeRepository.java
+++ src/main/java/lucashs/dev/repositories/CidadeRepository.java
@@ -1,18 +1,14 @@
 package lucashs.dev.repositories;
 
 import io.quarkus.hibernate.orm.panache.PanacheQuery;
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import io.quarkus.panache.common.Page;
 import jakarta.enterprise.context.ApplicationScoped;
 import lucashs.dev.common.PagedList;
 import lucashs.dev.entities.Cidade;
 
 @ApplicationScoped
-public class CidadeRepository implements PanacheRepository<Cidade> {
-
-    public Cidade findById(int id) {
-        return Cidade.findById(id);
-    }
+public class CidadeRepository implements PanacheRepositoryBase<Cidade, Integer> {
 
     public PagedList<Cidade> findByNome(String nome, Page page) {
         PanacheQuery<Cidade> pagedQuery = Cidade.find("LOWER(nome) LIKE ?1",
